7935fb347c4c68b715e6deb48cfe07f773bfbad9
ilhanbozcan <ilhanbozcann@gmail.com>
2019-11-07 15:29:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/moviebrowser/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.moviebrowser;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements MovieFragment.OnMovieSelectionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void movieSelected(Movie item) {
+        Intent intent = new Intent(this,DetailsActivity.class);
+        intent.putExtra("movie",item);
+        startActivity(intent);
+    }
+}
